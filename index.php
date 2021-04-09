f6ef87cb3f8e08ad9f13ae5031dba2e0d47dbb50
Chetan Bansal <chetanbansal1611@gmail.com>
2021-04-09 22:35:36 +0530

    fF

--- index.php
+++ index.php
@@ -12,21 +12,21 @@ if(isset($_POST['submit'])){
 	$msg="";
 	if($message == '')
 	{ 		
-			$mail=new PHPMailer;
-			$mail->isSMTP();
-			$mail->Host='smtp.gmail.com;';
-			$mail->SMTPAuth=true;
-			$mail->Username = 'tuedcell@gmail.com';
-			$mail->Password = 'ExecutiveBoard2020-21';
-			$mail->SMTPSecure = 'tls';
-			$mail->Port = 587;
-			$mail->setFrom('tuedcell@gmail.com', 'EDC-TIET');
-			$mail->addAddress($email);
-			$mail->addReplyTo('tuedcell@gmail.com', 'EDC-TIET');
-			$mail->isHTML(true);
-			$mail->Subject = 'EDC ';
-			$mail->Body    = '<b>You have been successfully registered.<b>';
-            $mail->send();
+//			$mail=new PHPMailer;
+//			$mail->isSMTP();
+//			$mail->Host='smtp.gmail.com;';
+//			$mail->SMTPAuth=true;
+//			$mail->Username = 'tuedcell@gmail.com';
+//			$mail->Password = '';
+//			$mail->SMTPSecure = 'tls';
+//			$mail->Port = 587;
+//			$mail->setFrom('tuedcell@gmail.com', 'EDC-TIET');
+//			$mail->addAddress($email);
+//			$mail->addReplyTo('tuedcell@gmail.com', 'EDC-TIET');
+//			$mail->isHTML(true);
+//			$mail->Subject = 'EDC ';
+//			$mail->Body    = '<b>You have been successfully registered.<b>';
+//            $mail->send();
 			$msg="You have been successfully registered.";
 	}
 	else{
